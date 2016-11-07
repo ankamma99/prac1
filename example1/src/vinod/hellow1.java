c249332c0c0a643764f302ac3b48c0e40a3e2103
ankam <ankam@172.16.57.59>
2016-11-07 14:52:36 -0500

    commit1

---
+++ example1/src/vinod/hellow1.java
@@ -0,0 +1,23 @@
+package vinod;
+
+public class hellow1 {
+	
+	public int printprice(int a)
+	{
+		int i=100;
+		int result = i+a;
+		return result;
+		
+	}
+	
+	
+	public static void main(String args[])
+	{
+		
+		
+		System.out.println("method calling from package:");
+
+		
+	}
+
+}
